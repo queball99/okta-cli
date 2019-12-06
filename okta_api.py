a0dee965935e33d19727a1c3574d7bc40a9b9c39
Jim <jim@interlaced.io>
2019-12-05 23:45:46 -0500

    fixing auth

--- okta_api.py
+++ okta_api.py
@@ -13,16 +13,18 @@ class okta_api:
 		settingsFile = os.path.join(os.path.dirname(__file__), 'Resources/settings.json')
 		with open(settingsFile,'r') as sf:
 			settings = json.load(sf)
-		client_id = settings['client_id']
-		client_secret = settings['client_secret']
+		# client_id = settings['client_id']
+		# client_secret = settings['client_secret']
+		api_auth = settings['auth']
 		endpoint = 'https://dev-246301.okta.com/oauth2/default/v1/token'
 		headers = {
 			"Accept" : "application/json",
-			"Content-Type" : "application/x-www-form-urlencoded"
+			"Content-Type" : "application/x-www-form-urlencoded",
+			"Authorization" : "Basic %s" % api_auth
 		}
 		data = {"grant_type": "password","scope": "profile openid"}
 
-		r = requests.post(endpoint, headers=headers, auth=HTTPDigestAuth(client_id, client_secret), data=data)
+		r = requests.post(endpoint, headers=headers, data=data)
 		code = r.status_code
 		print(code)
 		response = r.json()
